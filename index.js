7d5a500eb5159ddc9de90ea8d6fa6cbc3adcade1
journeywithcode0786 <newjourney78692@gmail.com>
2024-02-20 15:47:09 +0530

    both Approach

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //concatination of two srtings //
-
-let str1="Md Israr";
-let str2="Ahemed";
-let result = str1.concat(" ",str2);
-console.log(result)
+// let str1="Md Israr";
+// let str2="Ahemed";
+// let result = str1.concat(" ",str2);  //using of concat() method 
+// console.log(result)
+//Anither approach//
+// let str1="Md Israr";
+// let str2="Ahemed";
+// console.log(str1+ " "+ str2)

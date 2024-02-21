dfe91fc67b4e4532e355ed523b06e2fd7ab9fdbc
journeywithcode0786 <newjourney78692@gmail.com>
2024-02-21 11:58:19 +0530

    Some method od String Added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 //parameter me el spaerator denge "" or dusra string variable
 // console.log(result)
 // <------------------------------------------------------------------>
-//Anither approach//
+//Another approach//
 // let str1="Md Israr";
 // let str2="Ahemed";
 // console.log(str1+ " "+ str2)
@@ -26,3 +26,24 @@
 //pe hai to last wala index humara excluding hota hai to hum ek 
 //index badha ke likte hain to (n-1) 15 include ho jayega or huamra 
 //= "Script" jo required output tha wo Print ho jayega 
+//<------------------------------------------------------------------------->
+//Length property
+// let str="My name is ";
+// console.log(str.length)  kisi bhi string ki length 
+//pata karne ke liye use kar te hain length property
+//<------------------------------------------------------------------------------>
+//kisi bhi string lo upper Case me convert karna ho to
+//hum toUpperCase() method ka use kate hain
+// let str= "md israr ahmed" 
+// let upper=str.toUpperCase();
+// // console.log(upper);
+//<---------------------------------------------------------------------------------->
+//kisi bhi string lo lower Case me convert karna ho to
+//hum toLowerCase() method ka use kate hain
+// let str="MD ISRAR AHMED";
+// let lower= str.toLowerCase();
+// console.log(lower)
+//<------------------------------------------------------------------------------------->
+//
+
+
